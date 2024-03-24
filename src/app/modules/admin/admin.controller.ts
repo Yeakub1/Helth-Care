8d498932ab23a90dec7f182a9542e5e9aecdeec0
Yeakub1 <mryeakub10@gmail.com>
2024-03-25 04:47:40 +0600

    zod error validation

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,107 +1,72 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { AdminServices } from "./admin.services";
 import pick from "../../../shared/paick";
 import { adminFiltarableFields } from "./admin.constant";
 import SendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 
-const getAllFormDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const filter = pick(req.query, adminFiltarableFields);
-    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
-    const result = await AdminServices.getAllFromDB(filter, options);
-    SendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin Data Fetched Successfully!",
-      meta: result.meta,
-      data: result.data,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
-const getByIdFormDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const getAllFormDB: RequestHandler = catchAsync(async (req, res) => {
+  const filter = pick(req.query, adminFiltarableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await AdminServices.getAllFromDB(filter, options);
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Data Fetched Successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getByIdFormDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.getByIdFormDB(id);
-    SendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin Data Fetched By Id!",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
-const updateIntoDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+  const result = await AdminServices.getByIdFormDB(id);
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Data Fetched By Id!",
+    data: result,
+  });
+});
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.updateIntoDB(id, req.body);
-    SendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin Data Updated!",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
-const deleteFormDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+  const result = await AdminServices.updateIntoDB(id, req.body);
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Data Updated!",
+    data: result,
+  });
+});
+
+const deleteFormDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.deleteFormDB(id);
 
-    SendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin Data Delete Successfuly!",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  const result = await AdminServices.deleteFormDB(id);
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Data Delete Successfuly!",
+    data: result,
+  });
+});
 
-const softDeleteFormDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const softDeleteFormDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.softDeleteFormDB(id);
-    SendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin Soft Data Delete Successfuly!",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+
+  const result = await AdminServices.softDeleteFormDB(id);
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Soft Data Delete Successfuly!",
+    data: result,
+  });
+});
 
 export const AdminController = {
   getAllFormDB,
--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,11 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 
 const router = express.Router();
 
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      return next();
+    } catch (error) {
+      next(error);
+    }
+    next();
+  };
+
 router.get("/", AdminController.getAllFormDB);
 router.get("/:id", AdminController.getByIdFormDB);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(update), AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFormDB);
 router.delete("/soft/:id", AdminController.softDeleteFormDB);
 
